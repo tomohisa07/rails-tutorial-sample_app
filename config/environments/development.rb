b9b247ac069a066cf7f302f86b67674871d6e2c5
tomohisa07 <t.sakutomo07@gmail.com>
2021-09-15 05:24:28 +0000

    Improve development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Cloud9 への接続を許可する
+  config.hosts.clear
+  
 end
