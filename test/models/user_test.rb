d2d33adedd98ce14e8e91df86386908a10be0c3c
tomohisa07 <t.sakutomo07@gmail.com>
2021-09-23 06:05:14 +0000

    Improve test/models/user_test.rb add mail address validate

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,4 +30,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
 end
