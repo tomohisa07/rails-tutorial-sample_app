94729833c4b99d26090badc6ce2c1e9015eb36fd
tomohisa07 <t.sakutomo07@gmail.com>
2021-10-03 08:14:42 +0000

    Improve users_controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params) 
-    if @user.save
-      
+    if @user.sav
+      flash[:success] = "Welcome to the Sample App!"
+      #user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
