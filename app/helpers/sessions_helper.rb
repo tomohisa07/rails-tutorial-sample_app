c8031a761e74fc01fa9de559042eec347e03f560
tomohisa07 <t.sakutomo07@gmail.com>
2021-12-15 07:19:16 +0000

    Improve sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+    # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
