4391ba07d32e9fdeb1637c758086a411f1a5bea8
tomohisa07 <t.sakutomo07@gmail.com>
2021-12-15 05:42:26 +0000

    Improve controllers/sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
   end
 
   def destroy
