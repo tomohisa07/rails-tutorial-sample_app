8a6523cd20babc51a85b53e78f980d566b098dd8
tomohisa07 <t.sakutomo07@gmail.com>
2021-12-27 02:07:35 +0000

    Improve controllers/sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in(user)
       redirect_to user
