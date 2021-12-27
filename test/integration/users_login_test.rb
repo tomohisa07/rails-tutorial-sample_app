db113bd0c60f590ba3e29217b433989bf427a334
tomohisa07 <t.sakutomo07@gmail.com>
2021-12-27 02:04:42 +0000

    test/integration/users_login_test Add login with valid email/invalid password

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+  
   test "login with invalid information" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
